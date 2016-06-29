6b003c2d60c85a109dfa05c75913e7071b1b9123
Raquel Bujans <raquel@compass.com>
2016-06-28 22:34:04 -0400

    markers render!!!

--- src/es6/googleMapsFactory.js
+++ src/es6/googleMapsFactory.js
@@ -1,33 +1,36 @@
 angular.module('starter.services')
   .factory('GoogleMaps', function($cordovaGeolocation, $ionicLoading,
-      $rootScope, $cordovaNetwork, ConnectivityMonitor, googleMapsKey) {
+      $rootScope, $cordovaNetwork, ConnectivityMonitor, googleMapsKey, PinService) {
 
     var apiKey = false;
     var map = null;
 
+    function setMapWithLocation(latLng) {
+    var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      // Wait until the map is loaded
+      google.maps.event.addListenerOnce(map, 'idle', function() {
+        loadMarkers();
+        enableMap();
+      });
+    };
+
     function initMap() {
       var options = {timeout: 10000, enableHighAccuracy: true};
       $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
-
         var latLng = new google.maps.LatLng(
             position.coords.latitude, position.coords.longitude);
-
-        var mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-        // Wait until the map is loaded
-        google.maps.event.addListenerOnce(map, 'idle', function() {
-          // loadMarkers();
-          enableMap();
-        });
-
+        setMapWithLocation(latLng);
       }, function(error){
-        console.log("Could not get location");
+        console.log("Could not get location, falling back on default location");
+        var latLng = new google.maps.LatLng(40.768037, -73.975705);
+        setMapWithLocation(latLng);
       });
     }
 
@@ -47,8 +50,7 @@ angular.module('starter.services')
       });
 
       // This function will be called once the SDK has been loaded
-      window.mapInit = function() {
-        console.log('google cb');
+      window.googleMapsCb = function() {
         initMap();
       };
 
@@ -60,10 +62,10 @@ angular.module('starter.services')
       // Note the callback function in the URL is the one we created above
       if(apiKey) {
         script.src = 'http://maps.google.com/maps/api/js?key=' + apiKey
-            + '&callback=mapInit';
+            + '&callback=googleMapsCb';
       }
       else {
-        script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
+        script.src = 'http://maps.google.com/maps/api/js?callback=googleMapsCb';
       }
 
       document.body.appendChild(script);
@@ -78,23 +80,45 @@ angular.module('starter.services')
     }
 
     function loadMarkers() {
-      //Get all of the markers from our Markers factory
-      // Markers.getMarkers().then(function(markers){
-      //   console.log("Markers: ", markers);
-      //   var records = markers.data.result;
-      //   for (var i = 0; i < records.length; i++) {
-      //     var record = records[i];
-      //     var markerPos = new google.maps.LatLng(record.lat, record.lng);
-      //     // Add the markerto the map
-      //     var marker = new google.maps.Marker({
-      //         map: map,
-      //         animation: google.maps.Animation.DROP,
-      //         position: markerPos
-      //     });
-      //     var infoWindowContent = "<h4>" + record.name + "</h4>";
-      //     addInfoWindow(marker, infoWindowContent, record);
-      //   }
-      // });
+      // Get all of the markers from our Markers factory
+      // Markers.getMarkers().then(function(markers) {
+      PinService.approvedPins.$loaded().then(function(pins) {
+        console.log("Pins: ", pins);
+        // var records = markers.data.result;
+        for (var i = 0; i < pins.length; i++) {
+          var pin = pins[i];
+          var pinPos = new google.maps.LatLng(pin.lat, pin.lng);
+
+          // Add the markerto the map
+          var marker = new google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
+            position: pinPos
+          });
+
+          addInfoWindow(marker, buildContentString(pin), pin);
+        }
+      });
+    }
+
+    function buildContentString(pin) {
+      var output = '';
+      // if ($scope.user && $scope.PinService.isFavorite(pin, $scope.user.uid)) {
+      //   output += '<i class="icon ion-ios-star energized"></i>';
+      // }
+      if (pin.flagged) {
+        output += '<i class="icon ion-ios-flag assertive pb-overlay-icon"></i>';
+      }
+
+      output += "<a class='map-info-titleLink' href='#/tab/mapPins/" + pin.$id + "'>" +
+          (pin.name ? pin.name : pin.short_address) + "</a>";
+
+      // todo: show icon if currently open
+      // if (pin.opening_hours && pin.opening_hours.open_now) { }
+      output += "<div><a class='map-info-DirectionsLink' href='https://www.google.com/maps/place/" +
+          pin.address + "'>Directions</a></div>";
+
+      return "<div class='map-infoContent'>" + output + "</div>";
     }
 
     function addInfoWindow(marker, message, record) {
@@ -102,7 +126,7 @@ angular.module('starter.services')
         content: message
       });
 
-      google.maps.event.addListener(marker, 'click', function () {
+      google.maps.event.addListener(marker, 'click', function() {
         infoWindow.open(map, marker);
       });
     }
@@ -159,56 +183,3 @@ angular.module('starter.services')
     }
   });
 
-// function loadMarkers() {
-  // Get all of the markers from our Markers factory
-  // Markers.getMarkers().then(function(markers) {
-  // Pins.approvedPins.$loaded().then(function(pins) {
-  //   console.log("Pins: ", pins);
-  //   // var records = markers.data.result;
-  //   for (var i = 0; i < pins.length; i++) {
-  //     var pin = pins[i];
-  //     var pinPos = new google.maps.LatLng(pin.lat, pin.lng);
-
-  //     // Add the markerto the map
-  //     var marker = new google.maps.Marker({
-  //       map: map,
-  //       animation: google.maps.Animation.DROP,
-  //       position: pinPos
-  //     });
-
-  //     // var infoWindowContent = "<h4>" + record.name + "</h4>";
-  //     addInfoWindow(marker, buildContentString(pin), pin);
-  //   }
-  // });
-// }
-
-// function buildContentString(pin) {
-//   var output = '';
-//   // if ($scope.user && $scope.PinService.isFavorite(pin, $scope.user.uid)) {
-//   //   output += '<i class="icon ion-ios-star energized"></i>';
-//   // }
-//   if (pin.flagged) {
-//     output += '<i class="icon ion-ios-flag assertive pb-overlay-icon"></i>';
-//   }
-
-//   output += "<a class='map-info-titleLink' href='#/tab/mapPins/" + pin.$id + "'>" +
-//       (pin.name ? pin.name : pin.short_address) + "</a>";
-
-//   // todo: show icon if currently open
-//   // if (pin.opening_hours && pin.opening_hours.open_now) {
-//   // }
-//   output += "<div><a class='map-info-DirectionsLink' href='https://www.google.com/maps/place/" +
-//       pin.address + "'>Directions</a></div>";
-
-//   return "<div class='map-infoContent'>" + output + "</div>";
-// };
-
-// function addInfoWindow(marker, message, record) {
-//   var infoWindow = new google.maps.InfoWindow({
-//     content: message
-//   });
-
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infoWindow.open(map, marker);
-//   });
-// }
